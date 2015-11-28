25ab46f8a6b093c4f5c7d25c00db3b22fe85546f
Nephtali Ordaz <blak.gem@gmail.com>
2015-11-28 14:33:28 -0700

    inicio del sitio

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+$("body").ready(function(){
+
+	$(".mi-slider").slick({
+		autoplay:true,
+		autoplaySpeed:3000,
+		slidesToShow: 3,
+  		slidesToScroll: 3
+	});
+
+
+var h1 = $("#main-title");
+
+	h1.click(function(){
+		console.log("click");
+	});
+
+
+	var esto = $(this);
+
+
+
+	h1.click(function(){
+		$(this).toggleClass("sobre");
+	});
+
+});
+
+
+	
