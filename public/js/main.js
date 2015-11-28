eb4bb6be671f73be3f6e0e83f6e523e3e2aacd4b
Jonathan <junta90@hotmail.com>
2015-11-28 16:04:59 -0600

    Cambie index.html y main.js

--- public/js/main.js
+++ public/js/main.js
@@ -6,21 +6,6 @@ $("body").ready(function(){
 		slidesToShow: 3,
   		slidesToScroll: 3
 	});
-
-
-var h1 = $("#main-title");
-
-	h1.click(function(){
-		h1.html(" Bienvenido a queshow");
-	});
-
-
-	
-
-
-
-	
-
 });
 
 
